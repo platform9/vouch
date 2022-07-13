dd8980faa394e5b637fa4975e624ac775feea77d
zni0 <78295150+zni0@users.noreply.github.com>
2022-07-13 07:49:17 +0530

    Create a new API for active CA list
    Expose a new API that will list all the active CAs known to vouch. The list of
    CAs is obatined from consul data.

--- vouch/controllers/v1.py
+++ vouch/controllers/v1.py
@@ -1,4 +1,6 @@
 from vouch.controllers.sign import SignController
+from vouch.controllers.CA import ListCAController
 
 class V1Controller(object):
     sign = SignController()
+    cas = ListCAController()
