332db58b319a543ec440be7c143fd0c75e1bbf66
Mithil Arun <mithil@platform9.com>
2023-01-24 04:14:43 +0000

    switch to reading vault token from conf file

--- container/scripts/readiness_checks.sh
+++ container/scripts/readiness_checks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VAULT_URL=$(curl --header "X-Consul-Token: $CONSUL_HTTP_TOKEN" $CONSUL_HTTP_ADDR/v1/kv/customers/$CUSTOMER_ID/vouch/vault/url?raw 2>/dev/null)
-VAULT_TOKEN=$(curl --header "X-Consul-Token: $CONSUL_HTTP_TOKEN" $CONSUL_HTTP_ADDR/v1/kv/customers/$CUSTOMER_ID/vouch/vault/host_signing_token?raw 2>/dev/null)
+VAULT_TOKEN=$(grep vault_token /etc/vouch/vouch-keystone.conf | awk '{ print $2 }')
 
 TOKEN_HEALTH_STATUS=$(curl -o /dev/null -s -w "%{http_code}\n" --header "X-Vault-Token: $VAULT_TOKEN" $VAULT_URL/v1/auth/token/lookup-self)
 
