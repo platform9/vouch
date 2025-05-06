0662d65ee451ade0dc87aff5d4a423276036398e
Nilesh <nilesh_eng@platform9.com>
2025-05-06 15:19:14 +0530

    PCD-2002: Add vouch API to provide the keystone creds required by castellan

---
+++ vouch/controllers/creds.py
@@ -0,0 +1,52 @@
+# pylint: disable=too-few-public-methods
+
+import logging
+import pecan
+import re
+
+from pecan import expose
+from pecan.rest import RestController
+
+from vouch.conf import CONF
+from firkinize.configstore.consul import Consul
+
+LOG = logging.getLogger(__name__)
+
+def _json_error_response(response, code, exc):
+    """
+    json response from an exception object
+    """
+    response.status = code
+    response.content_type = 'application/json'
+    response.charset = 'utf-8'
+    try:
+        response.json = {'message': '%s: %s' % (exc.__class__.__name__, exc)}
+    except Exception:
+        response.json = {'message': 'Request Failed'}
+    return response
+
+class ListCredsController(RestController):
+    def __init__(self):
+        self._consul = Consul(
+            CONF['consul_url'],
+            CONF['consul_token'],
+        )
+        self._prefix = 'customers/%s' % (CONF['customer_id'])
+
+    @expose('json')
+    def get(self, user):
+        """
+        GET /v1/creds/<user>
+        Get the keystone credentials for a user
+        :param user: The user to get the credentials for 
+        :returns: 200 with the credentials
+        """
+        LOG.info('Fetching credentials for user')
+        try:
+            creds =  self._consul.kv_get_prefix(self._prefix+ '/keystone/users/%s/password' % user)
+            pecan.response.status = 200
+            pecan.response.json = creds 
+        except Exception as e:
+            LOG.error('Could not fetch credential from consul', e)
+            return _json_error_response(pecan.response, 500, e)
+        return pecan.response
--- vouch/controllers/v1.py
+++ vouch/controllers/v1.py
@@ -1,6 +1,8 @@
 from vouch.controllers.sign import SignController
 from vouch.controllers.CA import ListCAController
+from vouch.controllers.creds import ListCredsController
 
 class V1Controller(object):
     sign = SignController()
     cas = ListCAController()
+    creds = ListCredsController()
