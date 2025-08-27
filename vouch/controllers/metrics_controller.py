acc1143f8a2f74b635926d2807e2df7fcfc5d9b4
deepak07dbz <dchauhan@platform9.com>
2025-08-27 23:46:11 +0530

    expose vouch token expiry

--- vouch/controllers/metrics_controller.py
+++ vouch/controllers/metrics_controller.py
@@ -1,4 +1,5 @@
 
+import os
 import logging
 import pecan
 import time
@@ -11,11 +12,19 @@ from pecan.rest import RestController
 from vouch.conf import CONF
 from vaultlib.ca import VaultCA
 from prometheus_client import generate_latest, Gauge
+from datetime import datetime, timezone
+from dateutil import parser
+from firkinize.configstore.consul import Consul
+
 
 g_ca_cert_refresh_needed = Gauge('refresh_needed', 'Is CA cert refresh needed?')
 g_ca_cert_expiry_time = Gauge('cert_expiry_time', 'Time in seconds till CA cert expires')
+g_host_signing_token_expiry = Gauge('vouch_host_signing_token_expiry', 'Time in seconds till host signing token expires')
 
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(threadName)s - '
+                           '%(levelname)s - %(message)s')
 LOG = logging.getLogger(__name__)
 
 def query_vault(vault):
@@ -23,7 +32,7 @@ def query_vault(vault):
     cert = resp.json()['data']['certificate']
     c=x509.load_pem_x509_certificate(cert.encode('utf-8'),default_backend())
     return c
-    
+
 class  MetricsController(RestController):
     def __init__(self):
         self._vault = VaultCA(
@@ -36,6 +45,50 @@ class  MetricsController(RestController):
         cert = query_vault(self._vault)
         self.cert_expiration_time = cert.not_valid_after
 
+        # For querying host signing token expiry
+        self.vault_token = CONF['vault_token']
+        self.host_signing_token_expiry = None
+        self.host_signing_token_last_update_time = time.time()
+        self.customer_uuid = os.environ['CUSTOMER_ID']
+        config_url = os.environ.get('CONSUL_HTTP_ADDR', None)
+        token = os.environ.get('CONSUL_HTTP_TOKEN', None)
+        self.consul = Consul(config_url, token=token)
+    
+    def get_host_signing_token_expiry(self):
+        current_time = time.time()
+    
+        try:
+            vault_url = self.consul.kv_get(f'customers/{self.customer_uuid}/vouch/vault/url')
+            host_signing_token = self.consul.kv_get(f'customers/{self.customer_uuid}/vouch/vault/host_signing_token')
+
+            headers = {'X-Vault-Token': host_signing_token}
+
+            LOG.info("Sending request to Vault token lookup-self API...")
+            response = requests.get(f'{vault_url}/v1/auth/token/lookup-self', headers=headers)
+
+            if response.status_code != 200:
+                LOG.error(f"Vault token lookup failed: {response.status_code}, {response.text}")
+                return None
+
+            token_info = response.json()
+            expire_str = token_info['data'].get('expire_time')
+
+            if expire_str:
+                expire_time = parser.isoparse(expire_str)
+                current_time = datetime.now(timezone.utc)
+                ttl_seconds = int((expire_time - current_time).total_seconds())
+                self.host_signing_token_expiry = ttl_seconds
+                self.host_signing_token_last_update_time = current_time
+                LOG.info(f"Parsed expire time: {expire_time}")
+                self.host_signing_token_last_update_time = current_time
+            else:
+                LOG.info("expire_time not found in Vault response.")
+
+        except Exception as e:
+                LOG.error(f"Error while fetching host signing token expiry: {e}")
+                return None
+
+        return self.host_signing_token_expiry
 
     def get_cert_expiration_time(self):
         current_time = time.time()
@@ -52,9 +105,11 @@ class  MetricsController(RestController):
         """
         try:
             self.get_cert_expiration_time()
+            host_signing_token_expiry = self.get_host_signing_token_expiry()
             ca_cert_expiry_time = time.mktime(self.cert_expiration_time.timetuple())
             current_time = time.time()
             g_ca_cert_expiry_time.set(int(ca_cert_expiry_time))
+            g_host_signing_token_expiry.set(host_signing_token_expiry)
             if ca_cert_expiry_time - current_time < CONF['refresh_period']:
                 g_ca_cert_refresh_needed.set(1)
             else:
