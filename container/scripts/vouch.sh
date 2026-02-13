ad06adaec8c35f47bc167857e53de9f1fdf5027b
Thomas Dell <tdell@platform9.com>
2026-02-13 17:44:38 +0000

    noconsul changes

--- container/scripts/vouch.sh
+++ container/scripts/vouch.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+SELECTOR=$1
+
 ls -l /
 
 cat /templates/paste.ini | envsubst > /etc/vouch/paste.ini
-cat /templates/vouch-keystone.conf | envsubst > /etc/vouch/vouch-keystone.conf
-cat /templates/vouch-noauth.conf | envsubst > /etc/vouch/vouch-noauth.conf
+cat /templates/$SELECTOR.conf | envsubst > /etc/vouch/$SELECTOR.conf
 
-/usr/local/bin/vouch --config /etc/vouch/vouch-keystone.conf
+/usr/local/bin/vouch --config /etc/vouch/$SELECTOR.conf
