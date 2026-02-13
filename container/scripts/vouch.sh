6bdf3e67ae9da9fb537933ec4c3a1160ab0aaeb3
Thomas Dell <tdell@platform9.com>
2026-02-13 20:48:56 +0000

    noconsul changes

--- container/scripts/vouch.sh
+++ container/scripts/vouch.sh
@@ -7,4 +7,9 @@ ls -l /
 cat /templates/paste.ini | envsubst > /etc/vouch/paste.ini
 cat /templates/$SELECTOR.conf | envsubst > /etc/vouch/$SELECTOR.conf
 
-/vouch/vouch.py --config /etc/vouch/$SELECTOR.conf
+while true; do
+    echo "/vouch/vouch.py --config /etc/vouch/$SELECTOR.conf"
+    /vouch/vouch.py --config /etc/vouch/$SELECTOR.conf
+    echo vouch crashed! restarting
+    sleep 15
+done
