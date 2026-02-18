01139e451381e09c6341d5739702b1e1c712d8e7
Thomas Dell <tdell@platform9.com>
2026-02-18 00:40:51 +0000

    noconsul changes

--- container/scripts/vouch.sh
+++ container/scripts/vouch.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-SELECTOR=$1
-
 ls -l /
 
 cat /templates/paste.ini | envsubst > /etc/vouch/paste.ini
-cat /templates/$SELECTOR.conf | envsubst > /etc/vouch/$SELECTOR.conf
+cat /templates/$APP.conf | envsubst > /etc/vouch/$APP.conf
 
 while true; do
-    echo "/vouch/vouch.py --config /etc/vouch/$SELECTOR.conf"
-    /vouch/vouch.py --config /etc/vouch/$SELECTOR.conf
+    echo "/vouch/vouch.py --config /etc/vouch/$APP.conf"
+    /vouch/vouch.py --config /etc/vouch/$APP.conf
     echo vouch crashed! restarting
     sleep 15
 done
--- vouch/vouch.py
+++ vouch/vouch.py
@@ -11,10 +11,14 @@ async def ping(request):
 
 if __name__ == "__main__":
 
+    LOG.info("This is Radio Vouch")
+
     app_name = os.environ["APP"]
     if app_name == "vouch-keystone":
         port = 8448
     elif app_name == "vouch-noauth":
         port = 8558
+    else:
+        raise Exception("unknown APP: %s" % app_name)
 
     app.run(host="0.0.0.0", port=port, debug=True)
